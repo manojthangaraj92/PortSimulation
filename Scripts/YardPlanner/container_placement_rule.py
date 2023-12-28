ca83c1cdaa51a7cd62a42c756082d55a5f80b802
MANOJ KUMAR THANGARAJ <manoj@helgen.tech>
2023-12-28 22:48:40 +0000

    Container placement rule added, Folder refactored

---
+++ Scripts/YardPlanner/container_placement_rule.py
@@ -0,0 +1,21 @@
+from typing import List, Any
+from Scripts.Utils.containers import Container
+from Scripts.Utils.port_objects_definition import *
+from YardPlanner.blocks import Block
+
+class ContainerPlacementStrategy:
+    @staticmethod
+    def find_placement_by_bay(blocks:List[Block], 
+                       container:Container):
+        container_size = container._size
+        start_bay = 1 if container_size == ContainerSize.TWENTY_FT else 2
+        bay_increment = 2
+
+        for block in blocks:
+            for bay in range(start_bay, block.num_bays * 2, bay_increment):  # Adjusting for one-indexed list
+                for cell in range(1, block._num_cells + 1):
+                    if container_size == ContainerSize.TWENTY_FT and block._is_20ft_bay_available(bay, cell):
+                        return block, bay, cell
+                    elif container_size == ContainerSize.FORTY_FT and block._is_40ft_bay_available(bay, cell):
+                        return block, bay, cell
+        return None, None, None
--- Scripts/YardPlanner/yard_planner.py
+++ Scripts/YardPlanner/yard_planner.py
@@ -2,28 +2,20 @@ from Scripts.Utils.containers import Container
 from Scripts.Utils.port_objects_definition import *
 from YardPlanner.blocks import Block, BlockFactory
 from typing import List, Any, Tuple
+import simpy
+from YardPlanner.container_placement_rule import ContainerPlacementStrategy
 
 class YardPlanner:
-    def __init__(self):
-        self.blocks = []  # List to store blocks
+    def __init__(self, env:simpy.Environment):
+        self.env = env
+        self.blocks:BlockFactory = BlockFactory()  # List to store blocks
+        self.container_placement_rule:ContainerPlacementStrategy = ContainerPlacementStrategy()
 
-    def add_block(self, 
-                  block:Block) -> None:
-        self.blocks.append(block)
+    def add_block(self,
+                  capacity:int, 
+                  block_name:str) -> None:
+        self.blocks.get_block(self.env,
+                              capacity=capacity,
+                              name=block_name)
 
-class ContainerPlacementStrategy:
-    @staticmethod
-    def find_placement(blocks:List[Block], 
-                       container:Container):
-        container_size = container._size
-        start_bay = 1 if container_size == ContainerSize.TWENTY_FT else 2
-        bay_increment = 2
 
-        for block in blocks:
-            for bay in range(start_bay, block.num_bays * 2, bay_increment):  # Adjusting for one-indexed list
-                for cell in range(1, block._num_cells + 1):
-                    if container_size == ContainerSize.TWENTY_FT and block._is_20ft_bay_available(bay, cell):
-                        return block, bay, cell
-                    elif container_size == ContainerSize.FORTY_FT and block._is_40ft_bay_available(bay, cell):
-                        return block, bay, cell
-        return None, None, None
